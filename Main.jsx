d9af8ca72087ba254da0994959bd5b9f909312b6
Abhishek Kumar <sahniak56@gmail.com>
2023-02-02 02:22:53 +0530

    navigation added

---
+++ Main.jsx
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+const Main = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="home"
+                screenOptions={{
+                    headerShown: false,
+                    animation: 'slide_from_right',
+                }}
+            >
+                <Stack.Group>
+                    <Stack.Screen name="home" component={Home}  />
+                </Stack.Group>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Main
